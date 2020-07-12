e1a764df3f922327549a8191715d2be51980bdda
Trugamr <6trugamr9@gmail.com>
2020-07-12 17:33:08 +0530

    added methods to fetching and caching coin rates at start to save requests

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const API_KEY = '36F7F67E-08DE-4D08-BFCA-C39ED930DA5C';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +34,17 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future<Map> getCoinData(String coin) async {
+    try {
+      var response = await http.get(
+          'https://rest.coinapi.io/v1/exchangerate/$coin',
+          headers: {'X-CoinAPI-Key': API_KEY});
+
+      var jsonData = jsonDecode(response.body);
+      return jsonData;
+    } catch (e) {
+      throw 'failed to get coin data';
+    }
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 import 'dart:io' show Platform;
 import '../constants.dart';
 import '../coin_data.dart';
@@ -10,7 +11,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool _isLoading = true;
   String selectedCurrency = 'USD';
+  Map btcData, ethData, ltcData;
+  String btcRate = '?';
+  String ethRate = '?';
+  String ltcRate = '?';
 
   DropdownButton<String> getDropdownButton() {
     return DropdownButton<String>(
@@ -30,11 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ))
           .toList(),
       onChanged: (value) {
-        setState(
-          () {
-            selectedCurrency = value;
-          },
-        );
+        updateData(value);
       },
     );
   }
@@ -43,7 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return CupertinoPicker(
       itemExtent: 32.0,
       onSelectedItemChanged: (index) {
-        print(index);
+        updateData(currenciesList[index]);
       },
       children: currenciesList
           .map(
@@ -56,50 +58,119 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  void updateData(String currency) {
+    setState(() {
+      btcRate = btcData[currency].toStringAsFixed(2);
+      ethRate = ethData[currency].toStringAsFixed(2);
+      ltcRate = ltcData[currency].toStringAsFixed(2);
+      selectedCurrency = currency;
+    });
+  }
+
+  void getDataForCoins() async {
+    CoinData coinData = CoinData();
+    btcData = await coinData.getCoinData('BTC');
+    btcData = ratesListToMap(btcData['rates']);
+
+    ethData = await coinData.getCoinData('ETH');
+    ethData = ratesListToMap(ethData['rates']);
+
+    ltcData = await coinData.getCoinData('LTC');
+    ltcData = ratesListToMap(ltcData['rates']);
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    updateData('USD');
+  }
+
+  Map ratesListToMap(List rates) {
+    Map map = {};
+    rates.forEach((rate) {
+      map[rate['asset_id_quote']] = rate['rate'];
+    });
+    return map;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getDataForCoins();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Bitcoin Ticker'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 10,
-            child: Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Text(
-                        '1 BTC = ? USD',
-                        style: kTextStyle,
-                        textAlign: TextAlign.center,
+      body: LoadingOverlay(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 10,
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          '1 BTC = $btcRate $selectedCurrency',
+                          style: kTextStyle,
+                          textAlign: TextAlign.center,
+                        ),
                       ),
+                      color: Colors.blue,
+                      elevation: 5.0,
                     ),
-                    color: Colors.blue,
-                    elevation: 5.0,
-                  )
-                ],
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          '1 ETH = $ethRate $selectedCurrency',
+                          style: kTextStyle,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      color: Colors.blue,
+                      elevation: 5.0,
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          '1 LTC = $ltcRate $selectedCurrency',
+                          style: kTextStyle,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      color: Colors.blue,
+                      elevation: 5.0,
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(12.0),
-              color: Colors.blue,
+            Expanded(
+              flex: 2,
               child: Container(
-                child:
-                    Platform.isIOS ? getCupertinoPicker() : getDropdownButton(),
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(12.0),
+                color: Colors.blue,
+                child: Container(
+                  child: Platform.isIOS
+                      ? getCupertinoPicker()
+                      : getDropdownButton(),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+        isLoading: _isLoading,
       ),
     );
   }
